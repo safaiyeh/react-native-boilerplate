291c4771369907bcd106567a754ebc67765a7b8e
Jason Safaiyeh <jsafaiyeh@gmail.com>
2017-05-03 20:31:09 -0700

    Jest tests for App component

--- __tests__/src/App.js
+++ __tests__/src/App.js
@@ -1,8 +1,7 @@
 import 'react-native';
 import React from 'react';
-import Index from '../index.android.js';
+import Index from '../../src/App';
 
-// Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
--- src/App.js
+++ src/App.js
@@ -1,9 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   AppRegistry,
@@ -12,7 +6,7 @@ import {
   View
 } from 'react-native';
 
-export default class RNBoilerplate extends Component {
+export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
